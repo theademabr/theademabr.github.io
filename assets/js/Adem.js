e484508e90f929424911f1a7ac3f9dc469695b76
theademabr <124080501+theademabr@users.noreply.github.com>
2023-01-31 20:05:54 +0100

    gkhgu

---
+++ assets/js/Adem.js
@@ -0,0 +1,34 @@
+/*!
+=========================================================
+* Adem Landing page
+=========================================================
+
+* Copyright: 2019 Adem (https://theademabr.agency)
+* Licensed: (https://theademabr.agency/licenses)
+* Coded by www.theademabr.agency
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+$(document).ready(function () {
+  $(".nav-link").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        700,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
